89c4c1995b4d63008ff670dc76f212160154c9d0
nesanyas <119117345+nesanyas@users.noreply.github.com>
2023-02-04 15:44:53 +0300

    changes were made

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,8 +1,10 @@
 const open = document.querySelector("#burger");
 const openElement = document.querySelector("#active");
+const body = document.querySelector("#scrol")
 
 open.addEventListener("click", e => {
 openElement.style.display = "block";
+body.style.overflow = "hidden";
 })
 
 const closeElement = document.querySelector("#close");
@@ -10,5 +12,6 @@ const closeElement = document.querySelector("#close");
 closeElement.addEventListener("click", e => {
     e.preventDefault();
     openElement.style.display = "none";
+    body.style.overflow = "";
 })
 
